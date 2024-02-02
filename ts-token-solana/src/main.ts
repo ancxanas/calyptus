798663da6c7284a7dd0bb174dfc6225b1915aa61
Muhammed Anas <anas.corona.github@gmail.com>
2024-02-02 20:21:30 +0530

    Complete create token

---
+++ ts-token-solana/src/main.ts
@@ -0,0 +1,61 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { userKeyPair } from "./helper";
+import {
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import {
+  TokenStandard,
+  createFungible,
+  createV1,
+  mintV1,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+const umi = createUmi("https://api.devnet.solana.com");
+
+const keypair = umi.eddsa.createKeypairFromSecretKey(userKeyPair.secretKey);
+
+umi.use(keypairIdentity(keypair)).use(mplTokenMetadata());
+
+const metadata = {
+  name: "Solana Gold",
+  symbol: "GOLDSOL",
+  uri: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json",
+};
+
+const mint = generateSigner(umi);
+
+async function createMetadataDetails() {
+  await createV1(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 9,
+    tokenStandard: TokenStandard.Fungible,
+  }).sendAndConfirm(umi);
+}
+
+async function mintToken() {
+  await mintV1(umi, {
+    mint: mint.publicKey,
+    authority: umi.identity,
+    amount: 10_000,
+    tokenOwner: umi.identity.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+  }).sendAndConfirm(umi);
+}
+
+createFungible(umi, {
+  mint,
+  authority: umi.identity,
+  name: metadata.name,
+  symbol: metadata.symbol,
+  uri: metadata.uri,
+  sellerFeeBasisPoints: percentAmount(0),
+  decimals: 9,
+}).sendAndConfirm(umi);
